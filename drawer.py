f2b44bb169617b1538d5af982dc84556ebac2545
vanashimko <vana.shimko@gmail.com>
2017-02-02 22:10:28 +0300

    Improve drawing speed

--- drawer.py
+++ drawer.py
@@ -5,7 +5,9 @@ import numpy as np
 def display_result(vectors, clusters):
     colors = [np.random.rand(3, 1) for i in range(len(clusters))]
     for cluster_index, (centroid, cluster) in enumerate(clusters.items()):
+        current_cluster = [vectors[i] for i in cluster]
+        xs = list(map(lambda x: x[0], current_cluster))
+        ys = list(map(lambda x: x[1], current_cluster))
+        plt.scatter(xs, ys, c=colors[cluster_index])
         plt.scatter(centroid[0], centroid[1], c=colors[cluster_index], marker='x')
-        for vector_index in cluster:
-            plt.scatter(vectors[vector_index][0], vectors[vector_index][1], c=colors[cluster_index])
     plt.show()
