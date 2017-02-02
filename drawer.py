Fix bug with centroids calculation

drawer.py
@@ -12,8 +12,7 @@ def display_source(data):
 def display_result(vectors, clusters):
     colors = [np.random.rand(3, 1) for i in range(len(clusters))]
     for cluster_index, (centroid, cluster) in enumerate(clusters.items()):
-        plt.scatter(vectors[centroid][0], vectors[centroid][1], c=colors[cluster_index], marker='x')
+        plt.scatter(centroid[0], centroid[1], c=colors[cluster_index], marker='x')
         for vector_index in cluster:
-            if vector_index != centroid:
-                plt.scatter(vectors[vector_index][0], vectors[vector_index][1], c=colors[cluster_index])
+            plt.scatter(vectors[vector_index][0], vectors[vector_index][1], c=colors[cluster_index])
     plt.show()