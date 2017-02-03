1993ace59e506377402b748cb75cfcec3d8dc0aa
Ivan <vana.shimko@gmail.com>
2017-02-03 08:43:11 +0300

    Improve generator

--- generator.py
+++ generator.py
@@ -5,10 +5,11 @@ from sys import argv
 if __name__ == '__main__':
     components_count = int(argv[1])
     max_vectors_count = int(argv[2])
-    file_name = argv[3]
+    high_bound = int(argv[3])
+    file_name = argv[4]
     result = set()
     for i in range(max_vectors_count):
-        result.add((randint(0, 100) for i in range(components_count)))
+        result.add(tuple(randint(0, high_bound) for i in range(components_count)))
     with open(file_name, 'w') as out:
         out.write('{}\n'.format(components_count))
         for item in result:
